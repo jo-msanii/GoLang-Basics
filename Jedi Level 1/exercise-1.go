e87fa0b7b78b7c0e1f1e109b8675dcbc05fe442f
jo-msanii <job.ballard@gmail.com>
2022-04-08 12:20:38 -0700

    exercise 2

---
+++ Jedi Level 1/exercise-1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Print("x = ", x, ", y = ", y, ", z = ", z, "\n")
+	fmt.Printf("x = %v, y = %v, z = %v \n", x, y, z)
+}
