6cf5e074d945c045e8cf8fb85f810e3632326d0d
jo-msanii <job.ballard@gmail.com>
2022-05-19 09:17:51 -0500

    ex 5

--- Golang Dojo/Auth/main.go
+++ Golang Dojo/Auth/main.go
@@ -18,16 +18,30 @@ func templating(W http.ResponseWriter, fileName string, data interface{}) {
 	t.ExecuteTemplate(W, fileName, data)
 }
 
+func signInUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func signUpUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func userHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
+	case "/sign-in":
+		signInUser(w, r)
+	case "/sign-up":
+		signUpUser(w, r)
 	case "/sign-in-form":
 		getSignInPage(w, r)
 	case "/sign-up-form":
 		getSignUpPage(w, r)
 	}
+
 }
 
 func main() {
 	http.HandleFunc("/", userHandler)
+	http.Handle("/css", http.FileServer(http.Dir("css/")))
 	http.ListenAndServe("", nil)
 }
